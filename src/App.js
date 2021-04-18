f4c78090cca7d47dbe76fc023c2221742491cf82
venkat <vramanalj@gmail.com>
2021-04-18 16:04:57 +0530

    removing unused libraries

--- src/App.js
+++ src/App.js
@@ -193,14 +193,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Sudoku
+        Sudoku.
       </header>
       <div className="App-content">
         <aside className="rulesStats has-text-left">
           <div className="content">
-            <h5 className="title is-size-6	">Rules </h5>
-            <div className="pl-5">
-              <p>The goal of Sudoku is to fill in a 9×9 grid with digits so that each column, row, and 3×3 section contain the numbers between 1 to 9. 
+            <h5 className="title is-size-6">Rules </h5>
+            <div className="pl-5 is-size-5">
+              <p className="has-text-justified">The goal of Sudoku is to fill in a 9×9 grid with digits so that each column, row, and 3×3 section contain the numbers between 1 to 9. 
                 At the beginning of the game, the 9×9 grid will have some of the squares filled in. 
                 Your job is to use logic to fill in the missing digits and complete the grid. Don’t forget, a move is incorrect if:
               </p>
